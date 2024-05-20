9e6cb0552dbd2df4dad937756b097666a742826a
Gheorghe-Ioan Fodor <ionut.fodor@gmail.com>
2024-05-20 14:50:36 +0300

    feat: google oauth2

--- src/auth/guards/google-oauth.guard.ts
+++ src/auth/guards/google-oauth.guard.ts
@@ -3,13 +3,4 @@ import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
 export class GoogleOauthGuard extends AuthGuard('google') {
-
-  // async canActivate(context: ExecutionContext): Promise<boolean> {
-  //   // @ts-expect-error
-  //   const activate = (await super().canActivate(context)) as boolean;
-  //   const request = context.switchToHttp().getRequest();
-  //   // @ts-ignore
-  //   await super().logIn(request);
-  //   return activate;
-  // }
 }
