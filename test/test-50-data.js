3c89a8984852fc9748dd0effd347006215c9df10
Gheorghe-Ioan Fodor <ionut.fodor@gmail.com>
2024-06-17 13:10:27 +0300

    feat: k6 tests

---
+++ test/test-50-data.js
@@ -0,0 +1,63 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+const jwtToken = __ENV.JWT_TOKEN;
+
+export let options = {
+  vus: 50,
+  duration: '30s'
+};
+
+export default function() {
+  let url = `http://localhost:3000/api/data`;
+
+  let payload = JSON.stringify({
+    measurement: "lora.tts.smartparking.sp4",
+    projections: [
+      "ack",
+      "parking_slot_status"
+    ],
+    filters: [
+      {
+        filterField: "ack",
+        filterValue: "0",
+        filterOperation: "EQ",
+        operatorEnum: "OR"
+      },
+      {
+        filterField: "parking_slot_status",
+        filterValue: "1",
+        filterOperation: "EQ",
+        operatorEnum: "AND"
+      },
+      {
+        filterField: "time",
+        filterValue: "2024-03-19T20:43:20.418Z",
+        filterOperation: "LT",
+        operatorEnum: "AND"
+      }
+    ],
+    sorter: {
+      sortDirection: "DESC"
+    },
+    pagination: {
+      all: false,
+      offset: 100,
+      limit: 20
+    }
+  });
+
+  let params = {
+    headers: {
+      'Authorization': `Bearer ${jwtToken}`,
+      'Content-Type': 'application/json',
+      'accept': '*/*'
+    },
+  };
+
+  let res = http.post(url, payload, params);
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  })
+};
